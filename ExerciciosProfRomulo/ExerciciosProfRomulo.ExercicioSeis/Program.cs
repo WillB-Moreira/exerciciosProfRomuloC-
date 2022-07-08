cf697a7436486c4ed6caef4ab0b319d3c620a4aa
WillB-Moreira <wmup73@gmail.com>
2022-07-08 10:42:17 -0300

    inclusao de exercicios

---
+++ ExerciciosProfRomulo/ExerciciosProfRomulo.ExercicioSeis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.ExercicioSeis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * Exercícios 6
+             * Crie um algoritmo que mostre os 100 primeiros números pares.
+             */
+            int i;
+            i = 1;
+            while (i <= 100)
+            {
+                if(i % 2 == 0)
+                Console.WriteLine(i);
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExerciciosProfRomulo/ExerciciosProfRomulo.ExercicioSete/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.ExercicioSete
+{
+    internal class Program
+    {
+        /*static void Main(string[] args)
+        {
+            
+   
+            int i = 100;
+            while (i > 0)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+            Console.ReadKey();
+
+            */
+
+            
+           
+            public static int fatorial(int numero) {
+                Console.WriteLine("Digite um número inteiro qualquer");
+                int resultado = 1;
+                for (int i = 0; i < numero; i--)
+                {
+                    resultado = resultado * numero;
+                }
+                return resultado;
+            Console.ReadKey();
+            }
+            
+            }
+            
+        }
+
+    
+
